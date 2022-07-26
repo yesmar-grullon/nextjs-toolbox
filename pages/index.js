ba95eaa7586d440b772288f930277ce035c768e7
Yesmar A. Grullon <51163439+yesmar-grullon@users.noreply.github.com>
2022-07-25 22:18:08 -0400

    update
    changed title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
